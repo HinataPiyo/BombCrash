9a31c6ae28227765ded268c484aba9341da347c6
Hinata <tairo8291@gmail.com>
2025-06-11 14:21:21 +0900

    熟練度を削除

--- Assets/Scripts/SO/SkillSO.cs
+++ Assets/Scripts/SO/SkillSO.cs
@@ -2,22 +2,21 @@ using UnityEngine;
 
 public abstract class SkillSO : ScriptableObject
 {
-    [Header("手動(false)or自動(true)"), SerializeField] bool isAuto;       // 手動（false）か自動（true）か
-    [Header("カテゴリ"), SerializeField] Category category;
-    [Header("画像"), SerializeField] Sprite sprite;
-    [Header("名前"), SerializeField] new string name;
-    [Header("効果"), SerializeField] string effect;
-    [Header("レアリティ"), SerializeField] Rarity rarity;
-    [Header("レベル"), SerializeField] int level = 0;
-    [Header("クールタイム(CT)"), SerializeField] float coolTime;
-    bool isEndCoolTime = true; // クールタイムが終了しているかどうか
-    [Header("現在の熟練度"), SerializeField] int currentProficiency;
-    [Header("次の最大熟練度"), SerializeField] int maxProficiency;
-    [Header("熟練度の上昇率"), SerializeField] float proficiencyCostUpRate = 1.5f;
+    [Header("基本情報")]
+    [Tooltip("手動(false)or自動(true)"), SerializeField] bool isAuto;       // 手動（false）か自動（true）か
+    [Tooltip("カテゴリ"), SerializeField] Category category;
+    [Tooltip("画像"), SerializeField] Sprite sprite;
+    [Tooltip("名前"), SerializeField] new string name;
+    [Tooltip("効果"), SerializeField] string effect;
+    [Tooltip("レアリティ"), SerializeField] Rarity rarity;
+    [Tooltip("レベル"), SerializeField] int level = 0;
+    [Tooltip("クールタイム(CT)"), SerializeField] float coolTime;
+    bool isEndCoolTime = true;      // クールタイムが終了しているかどうか
+    
+    [Header("係数")]
     [Header("IPコストの上昇率"), SerializeField] float ipCostUpRate = 1.7f;
 
-
-    static readonly int[] ProficiencyBase = { 50, 75, 100, 150 };   // 熟練度のコスト
+    static readonly int[] BaseSkillStock = { 50, 75, 100, 150 };    // スキルの所持数(同種のスキル)
     static readonly int[] IPBaseCost = { 50, 75, 100, 125 };        // 知見ポイントのコスト
     public Category Category => category;
     public Sprite Icon => sprite;
@@ -28,16 +27,6 @@ public abstract class SkillSO : ScriptableObject
     public float CoolTime => coolTime;
     public bool IsEndCoolTime { get { return isEndCoolTime; } set { isEndCoolTime = value; } }
     public bool IsAuto { get { return isAuto; } set { isAuto = value; } }
-    public float CurrentProficiency => currentProficiency;
-    public float MaxProficiency => maxProficiency;
-
-    /// <summary>
-    /// 初期化処理
-    /// </summary>
-    public void Initialize()
-    {
-        maxProficiency = ProficiencyFetchCost();        // 熟練度の最大値を取得
-    }
 
     /// <summary>
     /// 知見ポイントをレアリティごとに決まった値を返す
@@ -52,57 +41,6 @@ public abstract class SkillSO : ScriptableObject
         // 0:N, 1:R, 2:SR, 3:SSR
         int cost = Mathf.FloorToInt(IPBaseCost[(int)rarity] * (ipCostUpRate * level));
         return cost;
-    }
-
-
-    /// <summary>
-    /// 熟練度をレアリティごとに決まった値を返す
-    /// </summary>
-    public int ProficiencyFetchCost()
-    {
-        if (level == 0)
-        {
-            return IPBaseCost[(int)rarity];
-        }
-        // レアリティごとに決まった値を返す
-        // 0:N, 1:R, 2:SR, 3:SSR
-        int cost = Mathf.FloorToInt(ProficiencyBase[(int)rarity] * (proficiencyCostUpRate * level));
-        return cost;
-    }
-
-    /// <summary>
-    /// 熟練度を上げる(スキルのレベルアップ)
-    /// </summary>
-    public void ProficiencyLevelUp()
-    {
-
-        level++;
-        currentProficiency = 0;
-        maxProficiency = ProficiencyFetchCost();
-
-        // スキルのレベルが上がった時の処理
-    }
-
-    /// <summary>
-    /// 熟練度のコストをチェックする
-    /// </summary>
-    public bool CheckProficiencyCost()
-    {
-        return currentProficiency >= maxProficiency;
-    }
-
-    /// <summary>
-    /// スキルのクールタイムをチェックする
-    /// </summary>
-    public void AddProficiency()
-    {
-        currentProficiency++;      // 熟練度を上げる
-        // 熟練度が最大値を超えた場合
-        if (currentProficiency > maxProficiency)
-        {
-            // 熟練度を最大値にする
-            currentProficiency = maxProficiency;
-        }
     }
     
     public abstract void Execute();     // スキルの実行処理
