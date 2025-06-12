5db0a16088cd03030cb6eaed984829501b1f566c
Hinata <tairo8291@gmail.com>
2025-06-13 05:11:15 +0900

    ホーム画面のセレクトボタンのアニメーションを修正

--- Assets/Scripts/UI/Animation/Home/HomeSelectButtonAnimation.cs
+++ Assets/Scripts/UI/Animation/Home/HomeSelectButtonAnimation.cs
@@ -6,22 +6,23 @@ using DG.Tweening;
 /// </summary>
 public class HomeSelectButtonAnimation : MonoBehaviour
 {
-    Vector3 startPos = new Vector3(0.5f, 0, 0);
     float moveDuration = 1f;
     float fadeDuration = 1f;
 
-    [SerializeField] GameObject[] buttons;
+    [SerializeField] RectTransform[] buttons;
+    [SerializeField] RectTransform[] parents;
     [SerializeField] CanvasGroup canvasGroup;
 
     void Start()
     {
         for (int ii = 0; ii < buttons.Length; ii++)
         {
-            buttons[ii].transform.position = new Vector3(buttons[ii].transform.position.x, startPos.y + ii * -2.5f, 0);
+            // 開始位置にセット
+            buttons[ii].anchoredPosition = new Vector3(0, (ii + 1) * -45);
 
-            // 最初に速く、だんだんと遅くなる Ease.OutQuad を適用
-            buttons[ii].transform.DOMove(buttons[ii].transform.parent.position, moveDuration)
-                 .SetEase(Ease.OutQuad);
+            // 元の位置にアニメーション
+            buttons[ii].DOAnchorPos(Vector2.zero, moveDuration)
+                .SetEase(Ease.OutQuad);
         }
 
         canvasGroup.alpha = 0;
