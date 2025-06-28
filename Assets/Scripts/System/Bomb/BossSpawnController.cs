33eeadeb6f0a0b422892270fdcd1d3e6fc1cdba6
Muthuki Konishi <muthukikoni@gmail.com>
2025-06-29 02:23:50 +0900

    エラーの修正

--- Assets/Scripts/System/Bomb/BossSpawnController.cs
+++ Assets/Scripts/System/Bomb/BossSpawnController.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 
 public class BossSpawnController : MonoBehaviour
 {
-    public static readonly int BossWaveCount = 25;   // ボスの出現wave数
+    public static readonly int BossWaveCount = 9;   // ボスの出現wave数
     [SerializeField] Transform spawnPoint;      // ボスの出現位置
     public bool IsBossSpawned { get; private set; }     // ボスが出現しているかどうか
-    GameObject bossPrefab;          // ボスのプレハブ
+    public GameObject bossPrefab;          // ボスのプレハブ
 
     public void SpawnBoss()
     {
+        Debug.Log("kita");
         if (IsBossSpawned) return;      // 
-        if (spawnPoint == null|| bossPrefab == null) return;
+        Debug.Log("rreturn");
+        if (spawnPoint == null || bossPrefab == null) return;
+        Debug.Log("kokoyo");
 
         // ボスを出現させる処理
         GameObject boss = Instantiate(bossPrefab, spawnPoint.position, Quaternion.identity);
         IsBossSpawned = true;
+        Debug.Log("kokoomade");
     }
 }
